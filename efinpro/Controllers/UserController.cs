d96b37c949183bbf4dd2f6cd8c4c6fda8069cdb1
Ramu N <ramun@goggery.com>
2019-12-26 08:52:45 +0530

    updated User list and new user template

---
+++ efinpro/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace efinpro.Controllers
+{
+    public class UserController : EFinProBaseController
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+
+    }
+}
