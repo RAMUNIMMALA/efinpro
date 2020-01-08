3c2089bf79aab1ae7b583044771bae3a338fcce0
Ramun <ramun@goggery.com>
2020-01-08 10:07:44 +0530

    EFP-004 - Active User list

---
+++ DataAccess/DA_Users.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Models;
+using PagedList;
+
+namespace DataAccess
+{    
+   public class DA_Users :Utility
+    {      
+        List<Users> listuser = null;
+        /// <summary>
+        /// Description:To get the active user list from database
+        /// Name:Bhargav krishna
+        /// </summary>
+        
+        public List<Users> GetUsers()
+        {
+            try
+            {
+                IDbDataParameter[] arrParameter = new IDbDataParameter[] { };
+                dsResultSet = DB_UTILITY.RunSP("USP_FETCH_USERSLIST", arrParameter);
+                if (ValidateResultSet(dsResultSet))
+                {
+                    listuser = OBJECT_UTILITY.GetConvertCollection<Users>(dsResultSet.Tables[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return listuser;
+        }
+    }
+}
--- efinpro/Controllers/UserController.cs
+++ efinpro/Controllers/UserController.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using PagedList;
+using PagedList.Mvc;
 using System.Web.Mvc;
+using Models;
+using DataAccess;
 
 namespace efinpro.Controllers
 {
     public class UserController : EFinProBaseController
     {
-        // GET: User
-        public ActionResult Index()
-        {
-            return View();
+        DA_Users _dausers = null;
+        List<Users> users = null;
+    
+        // GET: Active User List
+        public ActionResult Index(int? page)
+        {            
+            try
+            {
+                _dausers = new DA_Users();
+                users = _dausers.GetUsers();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.alertMessage = ErrorMessage;
+            }
+            return View(users.ToList().ToPagedList(page ?? 1,5));   
         }
-
         public ActionResult NewUser()
         {
             return View();
         }
-
     }
 }
