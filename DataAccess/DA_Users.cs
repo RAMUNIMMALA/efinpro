f394358e04dd09c12e9280112861527e1ae150b8
A Sruthi <sruthireddy8887@gmail.com>
2020-01-28 14:40:44 +0530

    1.changed mailid property to MailID 2.changed error message in  base Controllers/EFinproBaseController 3.Changed format of html in view page

--- DataAccess/DA_Users.cs
+++ DataAccess/DA_Users.cs
@@ -4,18 +4,18 @@ using System.Data;
 using Models;
 using PagedList;
 
-
-
 namespace DataAccess
-{    
-   public class DA_Users :Utility
-    {      
+{
+    public class DA_Users : Utility
+    {
         List<Users> listuser = null;
+        Users _users = null;
+        DataSet dtuser = null;
+
         /// <summary>
         /// Description:To get the active user list from database
         /// Name:Bhargav krishna
-        /// </summary>
-        
+        /// </summary>        
         public List<Users> GetUsers()
         {
             try
@@ -32,6 +32,31 @@ namespace DataAccess
                 throw ex;
             }
             return listuser;
+        }
+        /// <summary>
+        /// Description :Verify User Login  
+        /// Name:Sruthi A
+        /// </summary>
+        public Users VerifyUserLogin(string UserMailID, string Password)
+        {
+            _users = new Users();
+            try
+            {
+                IDbDataParameter[] arrParameter = new IDbDataParameter[]{
+                     DB_UTILITY.CreateParameter("@iMailId",DbType.String, ParameterDirection.Input,UserMailID),
+                     DB_UTILITY.CreateParameter("@iPassword", DbType.String, ParameterDirection.Input,Password)
+             };
+                dtuser = DB_UTILITY.RunSP("USP_FETCH_USERSLOGIN", arrParameter);
+                if (ValidateResultSet(dtuser))
+                {
+                    _users = OBJECT_UTILITY.GetConvert<Users>(dtuser.Tables[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return _users;
         }
     }
 }
