8599ccef6c3596b485d520134a82230dbb61a710
ramunimmala <ramun@goggery.com>
2020-02-03 12:21:21 +0530

    1.Adding CreateUser method in DA_Users 2.Adding getRandomAlphaNumericstring method in DA_Users 2.Adding NewUser ActionMethod in UserController 4.modify existing htmlcode in NewUser.cshtml

--- DataAccess/DA_Users.cs
+++ DataAccess/DA_Users.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using Models;
 using PagedList;
-
 namespace DataAccess
 {
     public class DA_Users : Utility
@@ -11,7 +10,6 @@ namespace DataAccess
         List<Users> listuser = null;
         Users _users = null;
         DataSet dtuser = null;
-
         /// <summary>
         /// Description:To get the active user list from database
         /// Name:Bhargav krishna
@@ -57,6 +55,56 @@ namespace DataAccess
                 throw ex;
             }
             return _users;
+        }
+        /// <summary>
+        /// Description :Insert User into Database 
+        /// Name:AjayKumar J
+        /// </summary>
+        public Users CreateUser(Users usr)
+        {
+            Users _user = null;
+            try
+            {
+                IDbDataParameter[] arrparams = new IDbDataParameter[]{
+                    DB_UTILITY.CreateParameter("@ic_FirstName",DbType.String,ParameterDirection.Input,usr.FirstName),
+                    DB_UTILITY.CreateParameter("@ic_LastName",DbType.String,ParameterDirection.Input,usr.LastName),
+                    DB_UTILITY.CreateParameter("@ic_MailID",DbType.String,ParameterDirection.Input,usr.MailID),
+                    DB_UTILITY.CreateParameter("@ic_Password",DbType.String,ParameterDirection.Input,getRandomAlphaNumericstring()),
+                    DB_UTILITY.CreateParameter("@ic_ContactNumber",DbType.String,ParameterDirection.Input,usr.ContactNumber),
+                    DB_UTILITY.CreateParameter("@ic_Role",DbType.Int16,ParameterDirection.Input,"1"),
+                };
+                dsResultSet = DB_UTILITY.RunSP("USP_INSERT_NEWUSER", arrparams);
+                if (ValidateResultSet(dsResultSet))
+                {
+                    _user = OBJECT_UTILITY.GetConvert<Users>(dsResultSet.Tables[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return _user;
+        }
+        public string getRandomAlphaNumericstring()
+        {
+            string PasswordLength = "8";
+            string NewPassword = "";
+            string allowedChars = "";
+            allowedChars += "1,2,3,4,5,6,7,8,9,0";
+            allowedChars += "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,";
+            allowedChars += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,";
+            char[] sep = { ',' };
+            string[] arr = allowedChars.Split(sep);
+            string IDString = "";
+            string temp = "";
+            Random rand = new Random();
+            for (int i = 0; i < Convert.ToInt32(PasswordLength); i++)
+            {
+                temp = arr[rand.Next(0, arr.Length)];
+                IDString += temp;
+                NewPassword = IDString;
+            }
+            return NewPassword;
         }
     }
 }
